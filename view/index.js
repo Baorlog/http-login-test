6c190a8d093b350253e4d1745f39633c9178c188
Bao Long <baolong.a1yh@gmail.com>
2022-01-04 16:31:43 +0700

    Include post credentials

--- view/index.js
+++ view/index.js
@@ -2,6 +2,15 @@ $(document).ready(() => {
     $(".login-form").submit((e) => {
         e.preventDefault();
         console.log("Nothing will ever happen");
-        window.location.href = "success.html";
+
+        $.post("/", {
+            "username": $("#username").val(),
+            "password": $("#password").val()
+        },
+        (data, status) => {
+            console.log(`data: ${data} \nstatus: ${status}`);
+        })
+
+        // window.location.href = "success";
     })
 })
